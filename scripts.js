c9b9408ce5b88481fe89ad94d0f3ed4797218bc7
aoumnad <152605695+aoumnad@users.noreply.github.com>
2024-07-19 08:43:17 +0000

    SMSOK

--- scripts.js
+++ scripts.js
@@ -11,28 +11,21 @@ function montrerMenu() {
     });
 }
 
-function CopyCode(button) {
-    // Find the <code> element that is a sibling of the button's parent
-      const codeElement = button.parentElement.nextElementSibling.querySelector('code');
-          // Get the text content of the <code> element
-          const codeText = codeElement.innerText.trim();
-          // Create a temporary textarea element to hold the text to copy
-          const tempTextArea = document.createElement('textarea');
-          // Set the value of the textarea to the code text
-          tempTextArea.value = codeText;
-          // Append the textarea to the body (it needs to be in the DOM to copy its content)
-          document.body.appendChild(tempTextArea);
-          // Select the content of the textarea
-          tempTextArea.select();
-          // Copy the selected content to the clipboard
-          document.execCommand('copy');
-          // Remove the temporary textarea from the DOM
-          document.body.removeChild(tempTextArea);
-          // Optionally, you can notify the user that the code has been copied
-          //alert('Code copié dans le presse-papier !');
-          button.textContent = "C'est fait !";
-              setTimeout(function () {
-                  button.textContent = "Copier";
-                  //window.getSelection().removeAllRanges(); // Annuler la sélection  
-              }, 1000);
-  }
+async function CopyCode(button) {
+    try {
+        // Récupérer le code à partir de l'élément <code>
+        const codeElement = button.parentElement.nextElementSibling.querySelector('code');
+        const code = codeElement.innerText;
+
+        // Utiliser l'API Clipboard pour copier le texte
+        await navigator.clipboard.writeText(code);
+
+        // Informer l'utilisateur que le code a été copié
+        button.innerText = 'Code Copié';
+        setTimeout(() => {
+            button.innerText = 'Copier';
+        }, 2000);
+    } catch (err) {
+        console.error('Échec de la copie : ', err);
+    }
+}
