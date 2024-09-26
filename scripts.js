07c8929c8018b822861864170d00763bcdbd198b
aoumnad <aoumnad@gmail.com>
2024-09-26 06:56:06 +0100

    fin navigation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function montrerMenu() {
     LeftPanel_ID.classList.add("visible");  // .toggle marche aussi
-    const liens = document.querySelectorAll("#LeftPanelMenu_ID");
+    const liens = document.querySelectorAll("#LeftPanelMenu_ID a");
     liens.forEach(element => {
         element.addEventListener("click", () => {
             LeftPanel_ID.classList.remove("visible");
@@ -27,5 +27,17 @@ async function CopyCode(button) {
         }, 2000);
     } catch (err) {
         console.error('Échec de la copie : ', err);
+    }
+}
+
+// Fonction pour montrer ou cacher les sous-rubriques
+function toggleMenu(menuId, element) {
+    const menu = document.getElementById(menuId);
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block"; // Montrer les sous-rubriques
+        element.classList.add("open"); // Ajouter la classe 'open' pour faire pivoter la flèche
+    } else {
+        menu.style.display = "none"; // Cacher les sous-rubriques
+        element.classList.remove("open"); // Retirer la classe 'open'
     }
 }
